b0fc45a1e31fb7c7e1ea372bad9bf25c4b183a54
Abviol <gogamerokiller@gmail.com>
2023-12-11 14:10:10 +0100

    Add: Header
    1) Add src/components/header.tsx component with the header
    2) Add src/scss/layout/_header.scss stylesheet for the header
    3) Import the .scss file to src/scss/style.scss
    4) Add padding-y mixing which is used in header.scss to src/scss/utilities/_mixins.scss
    5) Compile style.scss to src/css/style.css

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { app_title } from '../data/app.data';
+
+export function Header() {
+   return(
+      <header className='header'>
+         <div className="container">
+            <div className="navbar">
+               <div className="brand">
+                  <span className='brand__text'>
+                     {app_title}
+                  </span>
+               </div>
+            </div>
+         </div>
+      </header>
+   )
+}
