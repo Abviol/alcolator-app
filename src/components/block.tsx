39960780b64c840ae0daa9d7a9d0728b39c6a403
Abviol <gogamerokiller@gmail.com>
2023-12-12 09:17:16 +0100

    Add: Block Component
    Add new react-component and styles for it

---
+++ src/components/block.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from 'react';
+
+export function Block() {
+   return(
+      <div className="block block-question">
+         <div className="block__container">
+            <h4 className="block__title">Block title</h4>
+            <div className="block__content">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae corrupti optio hic deleniti corporis.
+            </div>
+         </div>
+      </div>
+   )
+}
