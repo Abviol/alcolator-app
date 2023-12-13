2eccb336f248ec10503cd44709250ad5dc27ce39
Abviol <gogamerokiller@gmail.com>
2023-12-13 11:04:25 +0100

    Add: Checkbox Component + Changes in Description
    1) Add Checkbox react-component and styles for it
    2) Prevent selecting text for Checkboxes and RadioButtons
    3) Add mixin for preventing selecting text
    4) Add checkbox color variable

---
+++ src/components/checkbox.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from 'react';
+
+interface CheckboxProps {
+   text: string
+   name: string
+   id: string
+}
+
+export function Checkbox({ text, name, id }: CheckboxProps) {
+   return (
+      <div className="checkbox">
+         <input type='checkbox' name={name} id={id} />
+         <label htmlFor={id} className="checkbox__label">{text}</label>
+         <span className="checkbox__button"></span>
+      </div>
+   )
+}
