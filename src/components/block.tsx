cc864fdc41081b9b84530909dfb8acb66a392a01
Abviol <gogamerokiller@gmail.com>
2023-12-15 15:38:37 +0100

    Update: Block Component

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,14 +1,26 @@
 ﻿import * as React from 'react';
 
+type Block = 'common' | 'warning'
 
 interface BlockProps {
    children: React.ReactNode
+   type?: Block
    title: string
 }
 
-export function Block({children, title}: BlockProps) {
+export function Block({children, type, title}: BlockProps) {
+
+   var blockClass;
+   switch(type) {
+      case 'warning':
+         blockClass = 'block block-warning';
+         break;
+      default: 
+         blockClass = 'block block-common';
+   }
+
    return(
-      <div className="block block-question">
+      <div className={blockClass}>
          <div className="block__container">
             <h4 className="block__title">{ title }</h4>
             <div className="block__content">
